10efcfd020ed80b978126808abebfe1ab31839a3
Patrick Ullrich <patricku@outpost.health>
2021-06-16 21:51:00 -0600

    init

---
+++ pages/marketing/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
+export default function Marketing() {
+  return (
+    <div>
+      Marketing<Button colorScheme="blue">Test</Button>
+    </div>
+  );
+}
